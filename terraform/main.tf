16db6fe9e2a6e845f7b713511c3906a54c949216
vanescho669 <vanescho@amazon.com>
2025-12-17 14:32:00 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ module "sandbox" {
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform, Retry"
   }
 
   custom_fields = {
